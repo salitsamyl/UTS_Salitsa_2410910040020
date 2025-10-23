ea01caf0d543b199db27e138fe880d7d85351f52
salitsamyl <salitsameylani@gmail.com>
2025-10-23 21:58:29 +0700

    update

--- lib/menu.dart
+++ lib/menu.dart
@@ -16,12 +16,35 @@ class Menu extends StatelessWidget {
                   width: double.infinity,
                   height: 100,
                   decoration: BoxDecoration(
-                    color: Colors.grey,
+                    color: const Color.fromARGB(255, 220, 220, 220),
                     borderRadius: BorderRadius.only(topRight: Radius.circular(30), bottomRight: Radius.circular(30))),
                 ),
+              ),
+              Row(
+                spacing: 10,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: EdgeInsetsGeometry.only(top: 30, left: 50),
+                    child: Image.asset("gambar/Logo.png", width: 80,),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 40, right: 150),
+                    child: Text("LKS MART", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 40, color: Colors.blueGrey),),
+                  )
+                ],
               )
             ],
-          )
+          ),
+
+          Padding(padding: EdgeInsetsGeometry.all(40),
+          child: Column(
+            children: [
+              Row(
+                text
+              )
+            ],
+          ),)
         ],
       ),
     );
