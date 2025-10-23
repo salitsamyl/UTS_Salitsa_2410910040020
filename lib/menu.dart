8c6c848fa229785498a564412d9170e37f67c7a6
salitsamyl <salitsameylani@gmail.com>
2025-10-23 21:44:40 +0700

    update

--- lib/menu.dart
+++ lib/menu.dart
@@ -10,10 +10,15 @@ class Menu extends StatelessWidget {
         children: [
           Stack(
             children: [
-              Container(
-                decoration: BoxDecoration(
-                  color: Colors.grey,
-                  borderRadius: BorderRadius.only(topRight: Radius.circular(2))),
+              Padding(
+                padding: const EdgeInsets.only(top: 30, right: 150),
+                child: Container(
+                  width: double.infinity,
+                  height: 100,
+                  decoration: BoxDecoration(
+                    color: Colors.grey,
+                    borderRadius: BorderRadius.only(topRight: Radius.circular(30), bottomRight: Radius.circular(30))),
+                ),
               )
             ],
           )
